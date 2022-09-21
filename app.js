6afa487d7a2930cebdd14140e2366a409519487f
Takahiro Iizuka <agdjgt@gmail.com>
2022-09-22 07:53:50 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
+      // console.log(this.$refs.userText);
+    },
+  },
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted()');
+  }
+});
+
+app.mount('#app');
+setTimeout(function() {
+  app.unmount('#app');
+}, 3000);
+
+
+const app2 = Vue.createApp({
+  template: `
+  <p>{{ message }}</p>
+  `,
+  data() {
+    return {
+      message: 'It works!'
+    }
+  }
+});
+
+app2.mount('#app2');
